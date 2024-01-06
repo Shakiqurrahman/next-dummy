0b40c098a6f414b27a2f0e0df8f4ff392d12096c
learnwithancor <ancorpoint0@gmail.com>
2024-01-07 00:55:49 +0600

    commited

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,8 +1,12 @@
-export default function Home() {
-  return (
-   <main className='max-w-[1280px] mx-auto'>
-    <h2 className='text-center font-bold text-4xl my-10'>Hello <span className='text-blue-500'>Programmers!!</span></h2>
+import React from 'react';
+
+const ContactPage = () => {
+    return (
+        <main className='max-w-[1280px] mx-auto'>
+    <h2 className='text-center font-bold text-4xl my-10'>Hello From <span className='text-blue-500'> Contact Page!!</span></h2>
     <p className='text-justify'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam mollitia cumque odio vero maiores quia recusandae qui et harum quas saepe ipsam expedita illo fuga explicabo cupiditate quidem doloribus iste hic, voluptates voluptate. Velit ab, vitae dolore ipsa quo dignissimos odio illum. Necessitatibus dicta soluta perspiciatis dolore fuga tempore quia harum perferendis obcaecati fugit aliquam ullam, quae iusto blanditiis totam rerum illo dolorum molestiae? Earum qui laborum recusandae, ad voluptas, ut dicta eligendi sunt suscipit aperiam laudantium! Repellat vel explicabo dicta ad beatae neque quaerat atque iusto illum error similique eos exercitationem natus corporis nulla iure, minus fuga adipisci totam nesciunt soluta perferendis sunt expedita! Quam facilis quasi ipsum qui non esse distinctio voluptas, aperiam, eius tempora odio nisi. Velit, nulla tempora vel natus veritatis quas ducimus, quaerat et ea odit laudantium, vero illo repellat aperiam consequatur. Doloribus enim ipsum pariatur, rem saepe, nemo voluptas, fugiat modi praesentium quae corrupti numquam obcaecati! Nobis beatae, excepturi quod nulla reprehenderit ducimus ex eos eum eligendi nesciunt harum ad dicta fugit hic libero, adipisci nisi. Dolor quibusdam corrupti eveniet tenetur voluptatibus, cumque iure quidem modi sapiente. Provident repudiandae minima voluptate laborum animi laboriosam, odit quos nemo nam iusto dolor ex modi delectus omnis id harum tempore tenetur reiciendis et consequatur incidunt? Alias, voluptate ut numquam pariatur quod corrupti. Repellendus culpa deserunt odit dolore assumenda tempora temporibus quae sint amet libero modi sunt debitis repellat aut dolores quos sequi voluptas rem nulla molestias, voluptatum quaerat similique itaque. Laborum, sit quos neque cupiditate magnam velit! Ut debitis assumenda voluptatem. Impedit, fugiat voluptate deleniti consequuntur doloremque minus perspiciatis animi praesentium incidunt aut iure exercitationem reprehenderit officia, facere error! Quibusdam facilis deleniti tempora amet autem, iure nemo dicta! Doloremque sint iusto, adipisci repellat est, fuga provident, ea ipsa deleniti voluptas rem atque facere architecto ex quos veniam.</p>
    </main>
-  )
-}
+    );
+};
+
+export default ContactPage;
